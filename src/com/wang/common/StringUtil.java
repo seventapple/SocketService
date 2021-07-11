7bcdbc207bf5910294e0f1a9505c345a48ba5da2
guoguang.wang <903159351@qq.com>
2021-07-11 20:29:34 +0800

    FEAT:jsonNode objectBean String 之间相互转化

--- src/com/wang/common/StringUtil.java
+++ src/com/wang/common/StringUtil.java
@@ -5,13 +5,194 @@ import java.io.IOException;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Objects;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class StringUtil {
+
+	public static final String JSON_PATH_SEPARATOR = "/";
+
 	public static boolean isNull(String str) {
 		return !(str != null && str.length() != 0);
 	}
 
+	// json格式的字符串转换成Bean
+	public static <T> T strToBean(String str, Class<T> clazz) {
+		if (isNull(str)) {
+			return null;
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(str, clazz);
+		} catch (IOException e) {
+			return null;
+		}
+	}
+
+	/**
+	 * Bean转换成json格式字符串
+	 * 
+	 * @param obj    对象
+	 * @param pretty 是否格式转换
+	 * @return
+	 */
+	public static String objectToJson(Object obj, boolean pretty) {
+		if (obj == null) {
+			return "{}";
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			if (pretty) {
+				return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+			} else {
+				return mapper.writeValueAsString(obj);
+			}
+		} catch (Exception e) {
+			return "{}";
+		}
+	}
+
+	/**
+	 * bean -> JsonNode
+	 */
+	public static <T> JsonNode parseJsonNode(T obj) {
+		ObjectMapper mapper = new ObjectMapper();
+		if (Objects.isNull(obj))
+			// need ver2.10.1
+//			return mapper.nullNode();
+			return null;
+		return mapper.valueToTree(obj);
+	}
+
+	/**
+	 * file -> ObjectBean
+	 */
+	public static <T> T toObject(File f, Class<T> clazz) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(f, clazz);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
+	 * String -> ObjectBean
+	 */
+	public static <T> T toObject(String str, Class<T> clazz) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(str, clazz);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
+	 * JsonNode -> ObjectBean
+	 */
+	public static <T> T toObject(JsonNode content, Class<T> clazz) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.treeToValue(content, clazz);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
+	 * 指定路径的值变更
+	 */
+	public static <T> JsonNode updateValue(JsonNode content, String path, T value) {
+		JsonNode objNode = content.deepCopy();
+		if (Objects.isNull(objNode) || Objects.isNull(value)) {
+			return objNode;
+		}
+		String[] fields = path.split(JSON_PATH_SEPARATOR);
+		JsonNode childNode = objNode;
+		for (int i = 0; i < fields.length; i++) {
+			if (Objects.isNull(childNode)) {
+				return objNode;
+			}
+			String field = fields[i];
+			if (childNode.isArray()) {
+				if (isNumeric(field)) {
+					childNode = childNode.get(Integer.valueOf(field));
+				}
+			} else {
+				childNode = childNode.get(field);
+			}
+		}
+		String key = fields[fields.length - 1];
+		if (Objects.nonNull(childNode)) {
+			if (childNode.isArray()) {
+				if (isNumeric(key)) {
+					int idx = Integer.valueOf(key);
+					ArrayNode arrayChile = ((ArrayNode) childNode);
+					if (arrayChile.has(idx)) {
+						arrayChile.remove(idx);
+						arrayChile.insert(idx, parseJsonNode(value));
+					}
+				}
+			} else if (childNode.has(key)) {
+				((ObjectNode) childNode).set(key, parseJsonNode(value));
+			}
+		}
+		return objNode;
+	}
+
+	public static <T> JsonNode updateValue(String content, String path, T value) {
+		JsonNode object = toObject(content, JsonNode.class);
+		return updateValue(object, path, value);
+	}
+
+	/**
+	 * 指定路径删除或[null]设定
+	 */
+	public static void removeNodeOrSetNull(JsonNode body, String parentPaht, String fieldName, boolean removeFlg) {
+		// body.isEmpty()
+		if (body.isNull() || isNull(fieldName)) {
+			return;
+		}
+		if (!isNull(parentPaht)) {
+			String[] fields = parentPaht.split(JSON_PATH_SEPARATOR);
+			for (String field : fields) {
+				if (!isNull(field)) {
+					if (body.isArray()) {
+						body = body.get(Integer.valueOf(field));
+					} else {
+						body = body.get(field);
+					}
+				}
+			}
+		}
+		ObjectNode objNode = (ObjectNode) body;
+		if (removeFlg) {
+			objNode.remove(fieldName);
+		} else {
+			objNode.putNull(fieldName);
+		}
+	}
+
+	/**
+	 * 是否为半角数字
+	 */
+	public static final boolean isNumeric(String str) {
+		Matcher matcher = Pattern.compile("^[0-9]+$").matcher(str);
+		return matcher.find();
+	}
+
+	// 配置文件读取相关↓↓↓
 	public static String getPropertyFilePath() {
 		String path = "";
 		try {
